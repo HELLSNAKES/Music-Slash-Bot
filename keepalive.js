754dfc2b2760abbe30acc02699f68490c5d381ed
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-10-19 17:47:07 +0700

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
 app.get("/", (req, res) => {
-    res.send("https://github.com/Harmonynos")
+    res.send("A fake server to fool Heroku")
 })
 const keepalive = () => {
     app.listen(port, "0.0.0.0", function () {
