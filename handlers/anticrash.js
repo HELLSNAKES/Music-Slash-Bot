4db36c24921afa18fa6c5a9780d2bc7f9f45cafe
HELLSNAKE <63496374+HELLSNAKES@users.noreply.github.com>
2021-10-20 09:34:02 +0700

    winston

--- handlers/anticrash.js
+++ handlers/anticrash.js
@@ -1,8 +1,19 @@
+const winston = require("winston")
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "./anticrash.log" })
+    ],
+    format: winston.format.printf(log => `[${new Date().toLocaleString()}] - [${log.level.toLowerCase()}] - ${log.message}`)
+})
 module.exports = (client) => {
     process.on("unhandledRejection", (reason, p) => {
         console.log(reason, p)
+        logger.error(reason, p)
     })
     process.on("uncaughtException", (err, origin) => {
         console.log(err, origin)
+        logger.error(err, origin)
     })
 }
