63dc415357d8626c0e4c21853b6212105ffeadcc
osamagmt <34580348+osamagmt@users.noreply.github.com>
2022-03-04 05:55:23 +0100

    Delete startbot.bat

--- startbot.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ====================================================
-echo               Start Bot Script
-echo               Author : HELLSNAKES
-echo ====================================================
-chcp 65001 > NUL
-npm start
